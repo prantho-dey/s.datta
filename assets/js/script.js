9358c9146599d9ab14a18695c1dd0dd208249a91
Prantho <pranthodey89@gmail.com>
2023-02-08 03:16:45 +0600

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,20 @@
 	// Mobile Menu
 	$('.hamburger').on('click', function(){
 		$('.mobile-menu-wrapper').addClass('menu-active');
+		$('body').addClass('menu-body');
 	})
 	$('.mobile-menu-close').on('click', function(){
 		$('.mobile-menu-wrapper').removeClass('menu-active');
+		$('body').removeClass('menu-body');
 	})
+
+	// Countdown
+	$('.odometer').appear(function (e) {
+		var odo = $(".odometer");
+		odo.each(function () {
+			var countNumber = $(this).attr("data-count");
+			$(this).html(countNumber);
+		});
+	});
 
 })(jQuery);
