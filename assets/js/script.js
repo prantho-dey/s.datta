a1b276bb7e509a5fc19aefbfdca1e76c87b89a5f
Prantho <pranthodey89@gmail.com>
2023-02-09 04:01:42 +0600

    third commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,4 +21,25 @@
 		});
 	});
 
+	$('.testimonial-wrapper').owlCarousel({
+		items:1,
+		loop:true,
+		margin:0,
+		autoHeight:true,
+		autoplay:true,
+		autoplayTimeout:5000,
+		autoplayHoverPause:true,
+		nav:true,
+		navText: [`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		
+		<path d="M19 12H5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		<path d="M12 19L5 12L12 5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		
+		</svg>`,`<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M5 12H19" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		<path d="M12 5L19 12L12 19" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+		</svg>
+		`]
+	})
+
 })(jQuery);
